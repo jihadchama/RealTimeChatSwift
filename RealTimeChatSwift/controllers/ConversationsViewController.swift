68164ed2de70827d849a9669a5380397429b33a4
Jihad El Chama <login+jihadchama@elo7.com>
2021-01-28 14:21:01 -0300

    configure LoginViewController and RegisterViewController, implements UIImagePicker to select profile picture

--- RealTimeChatSwift/controllers/ConversationsViewController.swift
+++ RealTimeChatSwift/controllers/ConversationsViewController.swift
@@ -4,5 +4,19 @@ class ConversationsViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		view.backgroundColor = .red
+	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		
+		let userIsLogged = UserDefaults.standard.bool(forKey: "logged_in")
+		
+		if !userIsLogged {
+			let vc = LoginViewController()
+			let nav = UINavigationController(rootViewController: vc)
+			nav.modalPresentationStyle = .fullScreen
+			present(nav, animated: false)
+		}
 	}
 }
